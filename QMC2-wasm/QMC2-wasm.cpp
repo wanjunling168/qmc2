b0286a80cec9150066c4c18b32d5087226c00e6b
Jixun Wu <jixun.moe@gmail.com>
2021-12-13 19:27:44 +0000

    feat: wasm build

---
+++ QMC2-wasm/QMC2-wasm.cpp
@@ -0,0 +1,34 @@
+﻿// QMC2-wasm.cpp : Defines the entry point for the application.
+//
+
+#include "QMC2-wasm.h"
+
+uintptr_t createInstWidthEKey(std::string ekey_b64) {
+	StreamCencrypt* stream = new StreamCencrypt();
+	KeyDec* key_dec = new KeyDec();
+	key_dec->SetKey(ekey_b64.c_str(), ekey_b64.size());
+	stream->SetKeyDec(key_dec);
+	delete key_dec;
+	return reinterpret_cast<uintptr_t>(stream);
+}
+
+size_t sizeof_qmc_detection() {
+	return sizeof(qmc_detection);
+}
+
+bool detectKeyEndPosition(uintptr_t result, uintptr_t buf, size_t len) {
+	return detect_key_end_position(
+		*reinterpret_cast<qmc_detection*>(result),
+		reinterpret_cast<uint8_t*>(buf),
+		len
+	);
+}
+
+void decryptStream(uintptr_t sc, uintptr_t buf, size_t offset, size_t len) {
+	// uint64_t offset, uint8_t* buf, size_t len
+	reinterpret_cast<StreamCencrypt*>(sc)->StreamDecrypt(
+		uint64_t(offset),
+		reinterpret_cast<uint8_t*>(buf),
+		len
+	);
+}
---
+++ public/app.js
@@ -0,0 +1,118 @@
+const DETECTION_SIZE = 40;
+
+// Process in 2m buffer size
+const DECRYPTION_BUF_SIZE = 2 * 1024 * 1024;
+
+let lastURL = "";
+
+const $prog = document.getElementById("prog");
+const $input = document.getElementById("input");
+const $dl = document.getElementById("download");
+const $player = document.getElementById("player");
+
+async function decryptMGG(mggBlob, name) {
+  const QMCCrypto = await QMCCryptoModule();
+
+  const detectionBlob = new Uint8Array(mggBlob.slice(-DETECTION_SIZE));
+
+  const detectionBuf = QMCCrypto._malloc(detectionBlob.length);
+  QMCCrypto.writeArrayToMemory(detectionBlob, detectionBuf);
+  const detectionResult = QMCCrypto._malloc(QMCCrypto.sizeof_qmc_detection());
+  const ok = QMCCrypto.detectKeyEndPosition(
+    detectionResult,
+    detectionBuf,
+    detectionBlob.length
+  );
+  const position = QMCCrypto.getValue(detectionResult, "i32");
+  const len = QMCCrypto.getValue(detectionResult + 4, "i32");
+  const detectionError = QMCCrypto.UTF8ToString(detectionResult + 8);
+
+  QMCCrypto._free(detectionBuf);
+  QMCCrypto._free(detectionResult);
+
+  if (ok) {
+    const decryptedSize = mggBlob.byteLength - DETECTION_SIZE + position;
+    const ekey = new Uint8Array(
+      mggBlob.slice(decryptedSize, decryptedSize + len)
+    );
+
+    const decoder = new TextDecoder();
+    const ekey_b64 = decoder.decode(ekey);
+    const hCrypto = QMCCrypto.createInstWidthEKey(ekey_b64);
+
+    $prog.max = decryptedSize;
+    const decryptedParts = [];
+    const buf = QMCCrypto._malloc(DECRYPTION_BUF_SIZE);
+    let bytesToDecrypt = decryptedSize;
+    let offset = 0;
+    while (bytesToDecrypt > 0) {
+      const blockSize = Math.min(bytesToDecrypt, DECRYPTION_BUF_SIZE);
+
+      const blockData = new Uint8Array(
+        mggBlob.slice(offset, offset + blockSize)
+      );
+      QMCCrypto.writeArrayToMemory(blockData, buf);
+
+      QMCCrypto.decryptStream(hCrypto, buf, offset, blockSize);
+
+      decryptedParts.push(QMCCrypto.HEAPU8.slice(buf, buf + blockSize));
+
+      offset += blockSize;
+      bytesToDecrypt -= blockSize;
+      $prog.value = offset;
+
+      await new Promise((resolve) => setTimeout(resolve));
+    }
+    QMCCrypto._free(buf);
+    // TODO: free hCrypto
+
+    const isOGG = /\.mgg\d?$/.test(name);
+    const isFLAC = /\.mflac\d?$/.test(name);
+    const newExt = isOGG ? "ogg" : isFLAC ? "flac" : "bin";
+    const newFileName = `${name.replace(/[^.]+$/, "")}${newExt}`;
+    return [
+      decryptedParts,
+      newFileName,
+      newExt === "bin" ? "appli" : `audio/${newExt}`,
+    ];
+  } else {
+    alert("ERROR: could not decrypt\n       " + detectionError);
+    return [];
+  }
+}
+
+async function main() {
+  $input.onchange = () => {
+    const file = $input.files[0];
+    const name = file.name;
+    const reader = new FileReader();
+    reader.addEventListener("load", (e) => {
+      if (lastURL) {
+        URL.revokeObjectURL(lastURL);
+      }
+
+      decryptMGG(e.target.result, name).then(
+        ([blobs, newFileName, mimeType]) => {
+          if (!blobs) return;
+
+          const blob = new Blob(blobs, {
+            type: mimeType,
+          });
+
+          const url = (lastURL = window.URL.createObjectURL(blob));
+          $player.src = url;
+
+          $dl.href = url;
+          $dl.textContent = newFileName;
+          $dl.download = newFileName;
+        }
+      ).catch((err) => {
+        alert("解密失败。\n" + err.message);
+        console.error(err);
+      });
+    });
+    reader.readAsArrayBuffer(file);
+  };
+}
+
+main();
